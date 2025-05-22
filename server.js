04f1eb7d1f0cae2a3efecee8c47943816e55fd7c
gedikhan <gedikhan@students.zhaw.ch>
2025-05-22 21:16:35 +0200

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Die Lernjournalabgabe steht bald an!');
+  res.send('Hello FS2025 DevOps Course! Die Lernjournalabgabe steht bald an! Letzte Runde mit GitHub Actions!');
 });
 
 app.listen(PORT, HOST);
